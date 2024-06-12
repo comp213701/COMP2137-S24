7c7a45882798313719db95125f8f0166c334e894
Professor S <comp2137-01@zonzorp.net>
2024-06-12 11:34:23 -0400

    whatever bro

--- printmyvar.sh
+++ printmyvar.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 #
 
-[ -v myvar ]  || echo "There is no variable named myvar"
-[ -v myvar ] || exit 1
+if cd /flooble; then
+    if touch foo; then 
+        echo "made foo"
+    else
+        echo failed to make foo
+        exit 1
+    fi
+else
+    echo failed to cd to flooble
+    exit 1
+fi
+
+if [ -v myvar ]; then
+    echo "There is no variable named myvar"
+    exit 1
+fi
+
 [ -n "$myvar" ] && echo "myvar has '$myvar' in it." || echo "myvar has nothing in it."
 
 
